0f58a29ee403b2b2c9ece762dbc42762d15a6b78
Ferdi265 <theferdi265@gmail.com>
2014-11-01 12:52:01 +0100

    Fixed foreverstartup (again)

--- foreverstartup.sh
+++ foreverstartup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#check if already started
-forever list | grep ppplz-server.js > /dev/null
-if [ $? -ne 0 ]; then
-	#start forever daemon
-	forever start -a -l $(pwd)/logs/foreverlog.txt ppplz-server.js
-fi
+cd $(dirname $0)
+forever start -a -l $(dirname $0)/logs/foreverlog.txt ppplz-server.js
